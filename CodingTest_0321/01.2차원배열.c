fde1991c89bf3073ccaf50ce1de2714854694107
Seungbeen <seungbin803@gmail.com>
2022-04-04 12:00:39 +0900

    더더 유연한 1차원배열

--- CodingTest_0321/01.2차원배열.c
+++ CodingTest_0321/01.2차원배열.c
@@ -10,15 +10,22 @@ int main(void)
 		// printf("%d ", arr[i]);
 	// }
 	
-	for (int i = 0; i < 12; i++)
-	{
-
-	}
+	// for (int i = 0; i < 12; i++)
+	// {
+		// printf("%d ", arr[i]);
+	// }
 
 	// sizeof(arr) 배열 arr의 크기 : 4*12 = 48
 	// sizeof(int) int형 변수의 크기 : 4
-	for (int i = 0; i < sizeof(arr) / sizeof(int); i++)
+	// for (int i = 0; i < sizeof(arr) / sizeof(int); i++)
+	// {
+		// printf("%d ", arr[i]); // 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12
+	// }
+
+	// sizeof(arr) 배열 arr의 크기 : 4*12 = 48
+	// sizeof(arr) 공간 하나의 크기 : 4
+	for (int i = 0; i < sizeof(arr) / sizeof(arr); i++)
 	{
-		printf("%d ", arr[i]);
+		printf("%d ", arr[i]); // 1
 	}
 }
