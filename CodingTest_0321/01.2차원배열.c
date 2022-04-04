0ee8c37d244833205831da0d4e47b9721f04f397
Seungbeen <seungbin803@gmail.com>
2022-04-04 11:55:34 +0900

    좀 더 유연한 반복문

---
+++ CodingTest_0321/01.2차원배열.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int arr[12] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12 };
+
+	// 실수 1 -> idx 범위 초과 + 2부터 출력
+	// for (int i = 1; i <= 12; i++)
+	// {
+		// printf("%d ", arr[i]);
+	// }
+	
+	for (int i = 0; i < 12; i++)
+	{
+
+	}
+
+	// sizeof(arr) 배열 arr의 크기 : 4*12 = 48
+	// sizeof(int) int형 변수의 크기 : 4
+	for (int i = 0; i < sizeof(arr) / sizeof(int); i++)
+	{
+		printf("%d ", arr[i]);
+	}
+}
