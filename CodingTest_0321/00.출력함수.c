657466592a4dbd818494ee1d0235d0af677fea87
Seungbeen <seungbin803@gmail.com>
2022-04-04 11:41:54 +0900

    master

--- CodingTest_0321/00.출력함수.c
+++ CodingTest_0321/00.출력함수.c
@@ -14,16 +14,16 @@ int main(void)
 	// printf("%s", mjyeol);
 
 	// mjyeol를 %c로 찍어보자
-	// printf("%c", mjyeol[0]);	// 'H'
-	// printf("%c", mjyeol[1]);	// 'e'
-	// printf("%c", mjyeol[2]);	// 'l'
-	// printf("%c", mjyeol[3]);	// 'l'
-	// printf("%c", mjyeol[4]);	// 'o'
-	// printf("%c", mjyeol[5]);	// ' '
-	// printf("%c", mjyeol[6]);	// 'W'
-	// printf("%c", mjyeol[7]);	// 'o'
-	// printf("%c", mjyeol[8]);	// 'r'
-	// printf("%c", mjyeol[9]);	// 'l'
+	// printf("%c", mjyeol[0]);		// 'H'
+	// printf("%c", mjyeol[1]);		// 'e'
+	// printf("%c", mjyeol[2]);		// 'l'
+	// printf("%c", mjyeol[3]);		// 'l'
+	// printf("%c", mjyeol[4]);		// 'o'
+	// printf("%c", mjyeol[5]);		// ' '
+	// printf("%c", mjyeol[6]);		// 'W'
+	// printf("%c", mjyeol[7]);		// 'o'
+	// printf("%c", mjyeol[8]);		// 'r'
+	// printf("%c", mjyeol[9]);		// 'l'
 	// printf("%c", mjyeol[10]);	// 'd'
 	// printf("%c", mjyeol[11]);	// null문자
 
