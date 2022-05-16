39c2d2c3cb1bb1f585291d885f7e1da52ddb7d1d
Mirim <seungbin803@gmail.com>
2022-05-16 11:04:05 +0900

    1차원 배열 복습

---
+++ CodingTest_0321/0516문제1.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int arr[12] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12 };
+	for (int i = 0; i < sizeof(arr) / sizeof(int); i++) {
+		printf("%d ", arr[i]);
+	}
+}
+
