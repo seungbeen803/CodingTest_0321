f183c52455bf5f579d54d1d2a4169cd5e834abc1
Mirim <seungbin803@gmail.com>
2022-05-23 11:12:38 +0900

    문자열 복사 유연하게

--- CodingTest_0321/04.문자열 복사하기.c
+++ CodingTest_0321/04.문자열 복사하기.c
@@ -1,17 +1,57 @@
 #include <stdio.h>
+# include <string.h>	// strlen() 함수 호출을 위해
 
-int main(void) 
+//int main(void) 
+//{
+//	// src에 있는 문자열의 내용을 dest에 복사하세요
+//	char dest[20];			// 복사할 공간
+//	char src[] = "Hello";	// 복사할 문자열
+//
+//	for (int i = 0; i < 6; i++)
+//	{
+//		dest[i] = src[i];
+//	}
+//
+//	for (int i = 0; i < 6; i++)
+//	{
+//		printf("%c", dest[i]);
+//	}
+//
+//	return 0;
+//}
+
+//int main(void)
+//{
+//	// src에 있는 문자열의 내용을 dest에 복사하세요
+//	char dest[20];			// 복사할 공간
+//	char src[] = "Hello";	// 복사할 문자열
+//
+//	for (int i = 0; i < src[i] != '\0'; i++)
+//	{
+//		dest[i] = src[i];
+//	}
+//
+//	for (int i = 0; i < 6; i++)
+//	{
+//		printf("%c", dest[i]);
+//	}
+//
+//	return 0;
+//}
+
+int main(void)
 {
 	// src에 있는 문자열의 내용을 dest에 복사하세요
 	char dest[20];			// 복사할 공간
 	char src[] = "Hello";	// 복사할 문자열
+	int length = strlen(src);
 
-	for (int i = 0; i < 6; i++)
+	for (int i = 0; i < length; i++)
 	{
 		dest[i] = src[i];
 	}
 
-	for (int i = 0; i < 6; i++)
+	for (int i = 0; i < length; i++)
 	{
 		printf("%c", dest[i]);
 	}
