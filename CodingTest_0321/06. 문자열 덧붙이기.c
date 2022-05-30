00145b8798a29af5bc95211f9eef4c901a38a94a
Mirim <seungbin803@gmail.com>
2022-05-30 12:17:09 +0900

    문자열 덧붙이기 유연하게

--- CodingTest_0321/06. 문자열 덧붙이기.c
+++ CodingTest_0321/06. 문자열 덧붙이기.c
@@ -7,17 +7,21 @@ void my_strcat(char* s1, char* s2)
 	// s1과 y의 인덱스 값이 5로 같다.
 	int s1_len = strlen(s1);
 	int s2_len = strlen(s2);
-	s1[5] = s2[0];
-	s1[6] = s2[1];
-	s1[7] = s2[2];
-	s1[8] = s2[3];
-	s1[9] = s2[4];
-	s1[10] = s2[5];
+	//s1[5+0] = s2[0];
+	//s1[5+1] = s2[1];
+	//s1[5+2] = s2[2];
+	//s1[5+3] = s2[3];
+	//s1[5+4] = s2[4];
+	//s1[5+5] = s2[5];
 
-	/*for (int i = s1_len; i < s1_len + s2_len; i++)
+	// 5의 의미는 strlen(s1), 6번 반복하는 의미는 strlen(s2)
+	for (int i = 0; i < s2_len; i++)
 	{
-		
-	}*/
+		// s1_len은 hello 문자열 길이는 5
+		// s2_len를 덧붙였을 때의 인덱스의 값이 5부터 시작
+		// helloyellow에서 hello의 문자열 길이 5와 y의 인덱스 값이 5로 같다
+		s1[s1_len + i] = s2[i];
+	}
 }
 
 int main(void)
