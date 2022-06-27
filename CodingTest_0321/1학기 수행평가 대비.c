6a4ea170add705c9274541ce11460b58fcb52c84
Mirim <seungbin803@gmail.com>
2022-06-27 10:41:52 +0900

    문자열의 길이구하기 strlen()

---
+++ CodingTest_0321/1학기 수행평가 대비.c
@@ -0,0 +1,13 @@
+#include <string.h>
+#include <stdio.h>
+
+int main(void)
+{
+	// 배열은 20개의 공간을 가지지만, 문자의 길이는 3
+	char str[20] = "YSY";
+
+	// 문자의 길이 구하는 함수
+	int length = strlen(str);
+	printf("%d \n", length);
+}
+
