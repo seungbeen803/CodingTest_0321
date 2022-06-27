f4f74a8ad36c5efdb4af98bbbd0132a51ed9643e
Mirim <seungbin803@gmail.com>
2022-06-27 10:51:58 +0900

    문자열 덧붙이기 strcat(덧붙일 공간, 덧붙일 문자내용)

--- CodingTest_0321/1학기 수행평가 대비.c
+++ CodingTest_0321/1학기 수행평가 대비.c
@@ -14,5 +14,9 @@ int main(void)
 	char copy_str[15];
 	strcpy(copy_str, str);
 	printf("%s \n", copy_str);
+
+	// 문자열 덧붙이기 strcat(덧붙일 대상, 덧붙일 문자열)
+	strcat(copy_str, "BLY");
+	printf("%s \n", copy_str);
 }
 
