c38c8df500c98ee49c02eb898e3a9f5b8051fd64
Mirim <seungbin803@gmail.com>
2022-06-27 10:56:42 +0900

    문자열 비교하기 strcmp strcmp 0을 반환해야 같은 글자

--- CodingTest_0321/1학기 수행평가 대비.c
+++ CodingTest_0321/1학기 수행평가 대비.c
@@ -18,5 +18,12 @@ int main(void)
 	// 문자열 덧붙이기 strcat(덧붙일 대상, 덧붙일 문자열)
 	strcat(copy_str, "BLY");
 	printf("%s \n", copy_str);
+
+	// 문자열 비교하기 strcmp
+	// 0일 경우 같다는 의미이다
+	if (strcmp(str, "YSY") == 0)
+		printf("양서영 입니다. \n");
+	else
+		printf("양서영이 아닙니다. \n");
 }
 
