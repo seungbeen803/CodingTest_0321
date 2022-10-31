586d239b620156b6546fac357d2dac2ef09c568b
Mirim <seungbin803@gmail.com>
2022-10-31 11:39:52 +0900

    02-05 기본 알고리즘 설계

---
+++ CodingTest_0321/구름에듀_기출문제/02-05.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+int solution(int attack, int recovery, int hp) {
+    int count = 0;
+
+    while (true) {
+        // count 1증가 (공격횟수)
+        // 몬스터를 때린다 (hp 30감소)
+        // 몬스터가 살아있으면
+            // hp회복 (10)
+        count += 1;
+        hp -= attack;
+        if (hp <= 0)
+            break;
+        hp += recovery;
+    }
+    return count;
+}
+
+int main() {
+    int attack = 30;
+    int recovery = 10;
+    int hp = 60;
+    int ret = solution(attack, recovery, hp);
+
+    printf("solution 함수의 반환 값은 %d 입니다.\n", ret);
+}
