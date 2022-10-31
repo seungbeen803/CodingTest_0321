a42b13c5e611420dbdc9770c6b8f66b450036f4d
Mirim <seungbin803@gmail.com>
2022-10-31 11:16:22 +0900

    02-04 일단 완성(정적할당)

--- CodingTest_0321/구름에듀_기출문제/02-04.c
+++ CodingTest_0321/구름에듀_기출문제/02-04.c
@@ -6,14 +6,17 @@
 // words 그냥 문자열이 들어가 있는 것
 // 5개 이상의 문자열만 걸러서 넣는다
 char* solution(char* words[], int words_len) {
-    char* answer = "";
+    // 단어의 개수 최대 100개 X 한 단어당 10개 + '/0' 공간 1개
+    char answer[1001] = "";
  
     // 길이 5이상의 문자열만 answer에 넣는다
     for (int i = 0; i < words_len; i++) {
         if (strlen(words[i]) >= 5)
             strcat(answer, words[i]);
     }
-    
+    // answer에 아무 글자도 없을 때, answer는 "empty"
+    if (strlen(answer) == 0)
+        strcat(answer, "empty");
 
 
     return answer;
