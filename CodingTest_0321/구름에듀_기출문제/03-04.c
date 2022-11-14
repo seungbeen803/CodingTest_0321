2f8fad4c89eccc56b34edccd8e2c826a104ae36a
Mirim <seungbin803@gmail.com>
2022-11-14 13:11:48 +0900

    03-04 유연하게

--- CodingTest_0321/구름에듀_기출문제/03-04.c
+++ CodingTest_0321/구름에듀_기출문제/03-04.c
@@ -7,33 +7,17 @@ int solution(char* words[], int words_len, char* word) {
     int count = 0;
     // words[0] : "CODE", words[1] : "COED", words[3] : "CDEO"
     for (int i = 0; i < words_len; i++) {
-        if (word[0] != words[i][0])
-            count++;         
-        if (word[1] != words[i][1])
-            count++;         
-        if (word[2] != words[i][2])
-            count++;
-        if (word[3] != words[0][3])
-            count++;
+        for (int j = 0; j < strlen(word); j++){
+            if (word[0] != words[i][0])
+                count++;
+            if (word[1] != words[i][1])
+                count++;
+            if (word[2] != words[i][2])
+                count++;
+            if (word[3] != words[i][3])
+                count++;
+        }
     }
-
-    if (word[0] != words[1][0])
-        count++;
-    if (word[1] != words[1][1])
-        count++;
-    if (word[2] != words[1][2])
-        count++;
-    if (word[3] != words[1][4])
-        count++;
-
-    if (word[0] != words[2][0])
-        count++;
-    if (word[1] != words[2][1])
-        count++;
-    if (word[2] != words[2][2])
-        count++;
-    if (word[3] != words[2][4])
-        count++;
 
     return count;
 }
