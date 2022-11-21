67864e091d2202dc4fcff0bb850af9c3c22c0590
Mirim <seungbin803@gmail.com>
2022-11-21 11:43:34 +0900

    03-07

--- CodingTest_0321/구름에듀_기출문제/03-07.c
+++ CodingTest_0321/구름에듀_기출문제/03-07.c
@@ -5,16 +5,25 @@
 
 int solution(int num_apple, int num_carrot, int k) {
     int answer = 0;
+    
+
+    // 먹이(k)를 주지 않았을때 만들 수 있는 주스의 개수(answer)
+    // 사과가 부족한 경우
     if (num_apple < 3 * num_carrot)
         answer = num_apple / 3;
     else
+        // 당근이 부족한 경우
         answer = num_carrot;
+    // 주스를 만들고 나서 남은 사과, 당근의 개수
     num_apple -= 3 * answer;
     num_carrot -= answer;
 
+    // 먹이를 주는 것까지 고려해서 만들 수 있는 주스의 개수
     for (int i = 0; k - (num_apple + num_carrot + i) > 0; i++)
+        // 과일이 4개 필요하기 때문에 4로 나누어준다
         if (i % 4 == 0)
-            answer++;
+            // 만들 수 있는 주스의 개수가 줄어들어야함
+            answer--;
     return answer;
 }
 
