99cfbb27e610fff75d2d36fbb19d5e844afea02e
Mirim <seungbin803@gmail.com>
2022-11-21 12:03:58 +0900

    03-08 완성

--- CodingTest_0321/구름에듀_기출문제/03-08.c
+++ CodingTest_0321/구름에듀_기출문제/03-08.c
@@ -3,16 +3,21 @@
 #include <stdlib.h>
 #include <string.h>
 
+// TV의 개수
 int solution(int programs[][2], int programs_len) {
     int answer = 0;
     int used_tv[25] = { 0, };
 
+    // 시간대별 방영한 tv의 개수를 count
     for (int i = 0; i < programs_len; i++)
+        // programs[i][0] : 시작시간, programs[i][1] : 끝나는 시간
         for (int j = programs[i][0]; j < programs[i][1]; j++)
+            // 방영시간을 나타냄
             used_tv[j]++;
 
+    // 하루에 TV를 2대 이상 트는 총 시간을 return 하도록
     for (int i = 0; i < 25; i++)
-        if (used_tv[i] >= 1)
+        if (used_tv[i] >= 2) // used_tv > 1도 가능
             answer++;
 
     return answer;
