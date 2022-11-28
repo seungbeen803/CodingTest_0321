88ea4ad827268546b59ed89a84f79dd2895d3571
Mirim <seungbin803@gmail.com>
2022-11-28 11:00:12 +0900

    3-10 완성

---
+++ CodingTest_0321/구름에듀_기출문제/03-10.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+/*
+* 2     in 4, 8, 3, 6, 3
+* 4     in 4, 8, 3, 6, 3
+* 1.5   in 4, 8, 3, 6, 3
+* 3     in 4, 8, 3, 6, 3
+* 1.5   in 4, 8, 3, 6, 3
+*/
+
+int solution(int arr[], int arr_len) {
+    int answer = 0;
+    for (int i = 0; i < arr_len; i++)
+        for (int j = 0; j < arr_len; j++)
+            // 배열의 요소 값은 짝수여야만 한다(2로 나누어도 정수가 되어야 한다)
+            // 자신을 2로 나눈 값(arr[i]/2)이 배열의 원소(arr[j])에 포함되어야 한다.
+            if (arr[i] % 2 == 0 && arr[i] / 2 == arr[j]) {
+                answer++;
+                break;
+            }
+    return answer;
+}return count;
+}
+
+int main() {
+    int arr[] = { 4, 8, 3, 6, 3 };
+    int arr_len = 5;
+    int ret = solution(arr, arr_len);
+
+    printf("solution 함수의 반환 값은 %d 입니다.\n", ret);
+}
