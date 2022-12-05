84f7da9df7e74748ca814350086aa4f4703e374c
Mirim <seungbin803@gmail.com>
2022-12-05 11:04:31 +0900

    04-05 수정 전

---
+++ CodingTest_0321/구름에듀_기출문제/04-05.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+
+int solution(int calorie[], int calorie_len) {
+    int min_cal = 0;
+    int answer = 0;
+    for (int i = 0; i < calorie_len; i++) {
+        // 현재값에-최솟값만큼 누적
+        if (calorie[i] > min_cal)
+            answer += calorie[i] - min_cal;
+        // 최솟값이 갱신
+        else
+            min_cal = calorie[i];
+    }
+    return answer;
+}
+
+int main() {
+    int calorie[] = { 713, 665, 873, 500, 751 };
+    int ret = solution(calorie, 5);
+
+    printf("solution 함수의 반환 값은 %d 입니다.\n", ret);
+}
